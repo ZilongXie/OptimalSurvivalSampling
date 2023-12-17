d1fc512990ce40bf22cc3c35699890f811a24cfd
Zilong Xie <19110180032@fudan.edu.cn>
2023-12-17 16:57:03 +0800

    Added comments.

--- cox_sampling.py
+++ cox_sampling.py
@@ -1,5 +1,3 @@
-import os
-import time
 import numpy as np
 import pandas as pd
 from numpy.linalg import inv
--- utils.py
+++ utils.py
@@ -4,7 +4,9 @@ def fun(x, pis):
     return np.min([np.array(x) * pis, np.ones(len(pis))], axis=0).sum()
 
 def find_position(nums, target):
-    # Use bisection to find the position of target
+    '''
+    Use bisection to find the position of target
+    '''
     left = 0
     right = len(nums) - 1
     mid = -1
@@ -22,8 +24,10 @@ def find_position(nums, target):
     else:
         return mid
 
-# Search for the optimal threshold given a seqence of probabilies and subsampling rate 
 def find_c(pis, r):
+    '''
+    Search for the optimal threshold given a seqence of probabilies and subsampling rate 
+    '''
     pis_temp = pis.copy()  
     pis_temp.sort()
     pis_temp = np.clip(pis_temp, 1e-10, None) # Modification to zero probabilities.
